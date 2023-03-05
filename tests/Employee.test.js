8beb3b576f087c03fd9de815128eb81a5e668e1d
HawaFallay <113000340+HawaFallay@users.noreply.github.com>
2023-03-05 15:50:24 -0800

    updated employeejs

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -4,43 +4,47 @@ const Employee = require('../lib/employee');
 //initialize testing
 
 describe("Employee", () =>{
-    describe("Initialiation", () => {
-        // object should return an empty string
-        it("should initiate an obeject", () => {
-            const obj = new Employee();
-            //'result' should equal object
-            expect(typeof(obj)).toEqual("object");
-    });
-});
+    descibe("Employee info", () => {
+        it("should return a string value", () => {
+            const strName = "Ronnie";
 
-//create test for name object 
-describe("Name", () => {
-    it("should create new name", () => {
-        const obj = new Employee("Ronnie");
-        expect(obj.name).toEqual("Ronnie");
-    });
-});
-//create test for id object
-describe("Id", () => {
-    it("should crete new id", () => {
-        const obj = new Employee("Ronnie", 11);
-        expect(obj.id).toEqual(11)
-    });
-});
+            const employee = new Employee(strName);
+            expect(employee.name).toBe(strName);
+        });
+         it("shouldshould return a string value", () => {
+            const strID = "11";
 
-//create test for email object
-describe("Email", () => {
-    it("should create new email", () => {
-        const obj = new Employee("Ronnie", 11, 'Ronnie11@gmail.com');
-        expect(obj.id).toEqual('Ronnie11@gmail.com')
+            const employee = new Employee("Ronnie", strId);
+            expect(employee.id).toBe(strId);
+        });
+
+        it("should return a string value", () => {
+            const strEmail = "Ronnie11@gmail.com";
+
+            const employee = new Employee("Ronnie", "11", strEmail);
+            expect(employee.email).toBe(strEmail);
         });
     });
+    
+    descibe("employee class methods", () => {
+        it ("should return a string value", () => {
+            const employee = new Employee("Ronnie", "11", "Ronnie11@gmail.com");
+            expect(employee.getName()).toBe("Ronnie");
+        });
 
-//create test for role object
-descibe("Role", () => {
-    it("should create new role", () => {
-        const obj = new Employee ("Ronnie", 11, 'Ronnie11@gmail.com', Employee)
-        expect(obj.role).toEqual(Employee)
+        it ("should return a string value", () => {
+            const employee = new Employee("Ronnie", "11", "Ronnie@gmail.com");
+            expect(employee.getId()).toBe("11");
+        });
+
+        it("should return a string value", () => {
+            const employee = new Employee("Ronnie", "11", "Ronnie11@gmail.com");
+            expect(employee.getEmail()).toBe("Ronnie11@gmail.com");
+        });
+
+        it("should return a string value", () => {
+            const employee = new Employee("Ronnie","11", "Ronnie11@gmail.com");
+            expect(employee.getRole()).toBe("Employee");
+        });
     });
-});
 });
